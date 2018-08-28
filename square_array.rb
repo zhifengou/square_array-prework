8f5df657e5ee370ef3a202d62b08ded708cef1db
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-28 18:42:38 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
-# def square_array(array)
-#   newArr=[]
-#   array.each do |i|
-#   newArr<<(i**2)
-# end
-# return newArr
-# end
 def square_array(array)
-  array.collect{|i| i**2}
+  newArr=[]
+  array.each do |i|
+  newArr<<(i**2)
+end
+return newArr
 end
